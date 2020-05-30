4fc6011c0010dfe842cd0def1b00dacb873abbf1
Josh Mu <mu@joshmu.com>
2020-05-30 14:53:53 +1000

    added localhost to whitelist

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3003
 app.use(morgan('tiny'))
 app.use(helmet())
 
-var whitelist = ['joshmu.com', 'mu-ceb-client']
+var whitelist = ['joshmu.com', 'mu-ceb-client', 'localhost']
 var corsOptions = {
   origin: function (origin, callback) {
     // check !origin for local development
